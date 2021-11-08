8e4de4d67aaf3eb5cb2a66d239c049e0754e733e
belladwir <belladwirh@gmail.com>
2021-11-08 13:02:36 +0700

    Initial Commit

---
+++ SimpleCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCalculator
+//
+//  Created by Bella Dwi Rahmatulia on 08/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
